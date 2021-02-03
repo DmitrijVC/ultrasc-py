380831e16c7039856764c6fe2183101738266dd6
Dmitrij <49795742+DmitrijVC@users.noreply.github.com>
2021-02-03 23:41:51 +0100

    change pkg name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="ultrasc-FssAy",
+    name="ultrasc",
     version="0.1.0",
     author="FssAy",
     author_email="FssAy.AF4@protonmail.com",
